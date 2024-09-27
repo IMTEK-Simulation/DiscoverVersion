f26a9fceba084d313329229d537c4a7fda7ad39d
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-09-27 13:05:27 +0200

    BUG: Need to pass safe.directory='*'

--- DiscoverVersion/Discovery.py
+++ DiscoverVersion/Discovery.py
@@ -27,8 +27,8 @@ import shutil
 import subprocess
 from pathlib import Path
 
-_toplevel_package = __name__.split('.')[0]
-_build_systems = ['flit_core']
+_toplevel_package = __name__.split(".")[0]
+_build_systems = ["flit_core"]
 
 
 class CannotDiscoverVersion(Exception):
@@ -39,40 +39,49 @@ def get_version_from_git(dirname):
     """
     Discover version from git repository.
     """
-    if shutil.which('git') is None:
-        raise CannotDiscoverVersion('git executable does not exist.')
+    if shutil.which("git") is None:
+        raise CannotDiscoverVersion("git executable does not exist.")
 
     path = Path(os.path.abspath(dirname))
-    while path.parent != path and not path.joinpath('.git').exists():
+    while path.parent != path and not path.joinpath(".git").exists():
         path = path.parent
 
-    if not path.joinpath('.git').exists():
-        raise CannotDiscoverVersion('.git directory does not exist.')
+    if not path.joinpath(".git").exists():
+        raise CannotDiscoverVersion(".git directory does not exist.")
 
     try:
         git_describe = subprocess.run(
-            ['git', 'describe', '--tags', '--dirty', '--always'],
+            [
+                "git",
+                "-c",
+                "safe.directory='*'",
+                "describe",
+                "--tags",
+                "--dirty",
+                "--always",
+            ],
             cwd=str(path),
-            stdout=subprocess.PIPE)
+            stdout=subprocess.PIPE,
+        )
     except FileNotFoundError:
         git_describe = None
     if git_describe is None or git_describe.returncode != 0:
-        raise CannotDiscoverVersion('git execution failed.')
-    version = git_describe.stdout.decode('latin-1').strip()
+        raise CannotDiscoverVersion("git execution failed.")
+    version = git_describe.stdout.decode("latin-1").strip()
 
-    dirty = version.endswith('-dirty')
+    dirty = version.endswith("-dirty")
 
     # Make version PEP 440 compliant
     if dirty:
-        version = version.replace('-dirty', '')
-    version = version.strip('v')  # Remove leading 'v' if it exists
-    version = version.replace('-', '.dev', 1)
-    version = version.replace('-', '+', 1)
+        version = version.replace("-dirty", "")
+    version = version.strip("v")  # Remove leading 'v' if it exists
+    version = version.replace("-", ".dev", 1)
+    version = version.replace("-", "+", 1)
     if dirty:
-        if '+' in version:
-            version += '.dirty'
+        if "+" in version:
+            version += ".dirty"
         else:
-            version += '+dirty'
+            version += "+dirty"
 
     return version
 
@@ -81,18 +90,20 @@ def get_version_from_pkginfo():
     """
     Discover version from PKG-INFO file.
     """
-    if not os.path.exists('PKG-INFO'):
-        raise CannotDiscoverVersion('PKG-INFO file does not exist.')
+    if not os.path.exists("PKG-INFO"):
+        raise CannotDiscoverVersion("PKG-INFO file does not exist.")
 
-    with open('PKG-INFO', 'r') as f:
+    with open("PKG-INFO", "r") as f:
         for line in f:
-            if line.startswith('Version: '):
+            if line.startswith("Version: "):
                 return line.split()[1]
 
-    raise CannotDiscoverVersion('Version not found in PKG-INFO.')
+    raise CannotDiscoverVersion("Version not found in PKG-INFO.")
 
 
-def get_version(package_name, file_name, use_git=True, use_importlib=True, use_pkginfo=True):
+def get_version(
+    package_name, file_name, use_git=True, use_importlib=True, use_pkginfo=True
+):
     """
     Discover version of package `package_name`.
 
@@ -112,17 +123,17 @@ def get_version(package_name, file_name, use_git=True, use_importlib=True, use_p
     Returns
     -------
     version : str
-        Version string.    
+        Version string.
     """
     discovered_version = None
-    tried = ''
+    tried = ""
 
     # We need to start with importlib because otherwise all packages will
     # have the version of the current git repository
 
     # inspect PKG-INFO file (if it exists)
     if discovered_version is None and use_pkginfo:
-        tried += ', PKG-INFO'
+        tried += ", PKG-INFO"
         try:
             discovered_version = get_version_from_pkginfo()
         except CannotDiscoverVersion:
@@ -130,7 +141,7 @@ def get_version(package_name, file_name, use_git=True, use_importlib=True, use_p
 
     # git works if we are in the source repository
     if discovered_version is None and use_git:
-        tried += ', git'
+        tried += ", git"
         try:
             if os.path.isdir(file_name):
                 dirname = file_name
@@ -141,9 +152,13 @@ def get_version(package_name, file_name, use_git=True, use_importlib=True, use_p
             discovered_version = None
 
     # importlib is present in Python >= 3.8
-    if discovered_version is None and _toplevel_package not in _build_systems and use_importlib:
+    if (
+        discovered_version is None
+        and _toplevel_package not in _build_systems
+        and use_importlib
+    ):
         try:
-            tried += ', importlib'
+            tried += ", importlib"
             from importlib.metadata import version
 
             discovered_version = version(package_name)
@@ -153,6 +168,6 @@ def get_version(package_name, file_name, use_git=True, use_importlib=True, use_p
     # Nope. Out of options.
 
     if discovered_version is None:
-        raise CannotDiscoverVersion(f'Tried: {tried[2:]}')
+        raise CannotDiscoverVersion(f"Tried: {tried[2:]}")
 
     return discovered_version
