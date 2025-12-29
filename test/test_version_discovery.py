c98c3bda9a8cbe143a4dfbed8ee59551f60b3f4f
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2025-12-29 12:01:41 +0100

    MAINT: Updated version to 0.3.1

--- test/test_version_discovery.py
+++ test/test_version_discovery.py
@@ -33,7 +33,7 @@ from tempfile import TemporaryDirectory
 from DiscoverVersion import __version__, get_version
 
 
-_current_version = "0.2.6"
+_current_version = "0.3.1"
 
 
 def test_version_discovery():
